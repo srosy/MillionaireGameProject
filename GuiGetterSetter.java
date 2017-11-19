de4999eaa35668a71c54dd42e311b51baeb4f17a
Trevyn <Trevyn@slave1.lan>
2017-11-19 14:40:05 -0700

    fixing the word wrap

--- GuiGetterSetter.java
+++ GuiGetterSetter.java
@@ -1,69 +1,70 @@
 /*********************************
-* Author: Spencer Rosenvall
-* Setters and getters to deliver
-* info from Phi's output to the Gui
-**********************************/
+ * Author: Spencer Rosenvall Setters and getters to deliver info from Phi's
+ * output to the Gui
+ **********************************/
 
-public class GuiGetterSetter{
+public class GuiGetterSetter
+{
 
-   private String currentQuestion;
-   private int currentRound;
-   private int moneyWon;
-   private int correctAnswer;
-   
+	private String currentQuestion;
+	private int currentRound;
+	private int moneyWon;
+	private int correctAnswer;
 
-   
-   // get variable with saved string question from program
-   public String getQuestion(){
-      return MillionaireApp.getcurrentQuestion;
-   }
-   
-   // get variable with saved int round number from program
-   public int getRound(){
-      return currentRound;
-   }
-   
-   // get variable with saved int correct answer from program
-   public int getAnswer(){
-      return correctAnswer;
-   }
-   
-   // get variable with saved int moneyWon from program
-   public int getMoneyWon(){
-      return moneyWon;
-   }
+	// get variable with saved string question from program
+	//	public String getQuestion()
+	//	{
+	//		return MillionairApp.getCurrentQuestion();
+	//	}
 
-   // get variable with saved array output from program
-   public int[] getArray(){
-      return false;
-   }
-   
-   
-   
-   
-   // save variable as current String from output for GUI
-   public void setQuestion(String s){
-      currentQuestion = s;
-   }
-   
-   
-   // save variable as current MoneyWon from output for GUI
-   public void setMoneyWon(int n){
-      moneyWon = n;
-   }
-   
-    // save variable as current Answer from output for GUI
-   public void setAnswer(int n){
-   
-   //I'm not sure if you have it aas a char, string, int, array, etc...
-      correctAnswer = n;
-   
-   }
+	// get variable with saved int round number from program
+	public int getRound()
+	{
+		return currentRound;
+	}
 
+	// get variable with saved int correct answer from program
+	public int getAnswer()
+	{
+		return correctAnswer;
+	}
 
+	// get variable with saved int moneyWon from program
+	public int getMoneyWon()
+	{
+		return moneyWon;
+	}
 
-   // save variable as current String from output for GUI
-   public void setQuestionRound(int n){
-      currentRound = n;  
-   }
+	// get variable with saved array output from program
+	//	public int[] getArray()
+	//	{
+	//		return false;
+	//	}
+
+	// save variable as current String from output for GUI
+	public void setQuestion(String s)
+	{
+		currentQuestion = s;
+	}
+
+	// save variable as current MoneyWon from output for GUI
+	public void setMoneyWon(int n)
+	{
+		moneyWon = n;
+	}
+
+	// save variable as current Answer from output for GUI
+	public void setAnswer(int n)
+	{
+
+		//I'm not sure if you have it aas a char, string, int, array, etc...
+		correctAnswer = n;
+
+	}
+
+	// save variable as current String from output for GUI
+	public void setQuestionRound(int n)
+	{
+		currentRound = n;
+	}
 }
--- MillionaireGUI.java
+++ MillionaireGUI.java
@@ -3,211 +3,201 @@
 * GUI for game play
 **********************************/
 
-
 /******************************************************************************************************************************
 * NOTES: 
 * -In getter/setters, use label.setText(var)to update the code, create update method that calls the setters and getters.
 * -Possibly add music thread
 * 
 *******************************************************************************************************************************/
-import javax.swing.*;
 import java.awt.Color;
 
-public class MillionaireGUI{
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
-MillionairApp app = new MillionairApp();
-//app.q1; //this line gives you access to the q1 object from the MillionaireApp class
+public class MillionaireGUI
+{
 
+	MillionairApp app = new MillionairApp();
+	//app.q1; //this line gives you access to the q1 object from the MillionaireApp class
 
-Question q = new Question();
-Price p = new Price();
+	Question q = new Question();
+	Price p = new Price();
 
-//public int questionNumber = object.getQuestionNumber(currentQuestionNum);
-//private String message = object.getMessageOutput(currentQuestion);
-private String moneyWon = p.getPrice();
+	//public int questionNumber = object.getQuestionNumber(currentQuestionNum);
+	//private String message = object.getMessageOutput(currentQuestion);
+	private String moneyWon = p.getPrice();
 
-private String message = app.q1.getQuestionContent();
-private String answerA = app.q1.getOption1();
-private String answerB = app.q1.getOption2();
-private String answerC = app.q1.getOption3();
-private String answerD = app.q1.getOption4();
+	private String message = app.q1.getQuestionContent();
+	private String answerA = app.q1.getOption1();
+	private String answerB = app.q1.getOption2();
+	private String answerC = app.q1.getOption3();
+	private String answerD = app.q1.getOption4();
 
-private String correctAnswer = q.getCorrectOption();
+	private String correctAnswer = q.getCorrectOption();
 
+	public JPanel createContentPane()
+	{
 
+		// We create a bottom-most JPanel to place everything on
+		JPanel totalGUI = new JPanel();
 
+		totalGUI.setLayout(null);
 
+		// Panel to contain the title labels
+		JPanel titlePanel = new JPanel();
+		titlePanel.setLayout(null);
+		titlePanel.setLocation(40, 10);
+		titlePanel.setSize(800, 100);
+		totalGUI.add(titlePanel);
 
-    public JPanel createContentPane (){
+		JLabel titleLabel = new JLabel("Who wants to be a Millionaire!");
+		titleLabel.setFont(titleLabel.getFont().deriveFont(40.0f));
+		titleLabel.setLocation(0, 0);
+		titleLabel.setSize(700, 100);
+		titleLabel.setHorizontalAlignment(0);
+		titleLabel.setForeground(Color.red);
+		titlePanel.add(titleLabel);
 
-        // We create a bottom-most JPanel to place everything on
-        JPanel totalGUI = new JPanel();
-        
-        totalGUI.setLayout(null);
+		JButton startButton = new JButton("Start");
+		startButton.setLocation(650, 40);
+		startButton.setSize(75, 25);
+		titlePanel.add(startButton);
 
-        // Panel to contain the title labels
-        JPanel titlePanel = new JPanel();
-        titlePanel.setLayout(null);
-        titlePanel.setLocation(40, 10);
-        titlePanel.setSize(800, 100);
-        totalGUI.add(titlePanel);
+		//Label for text output
+		JPanel messagePanel = new JPanel();
+		messagePanel.setLayout(null);
+		messagePanel.setLocation(75, 125);
+		messagePanel.setSize(600, 400); //600 x 400
+		totalGUI.add(messagePanel);
 
-        JLabel titleLabel = new JLabel("Who wants to be a Millionaire!");
-        titleLabel.setFont (titleLabel.getFont ().deriveFont (40.0f));
-        titleLabel.setLocation(0, 0);
-        titleLabel.setSize(700, 100);
-        titleLabel.setHorizontalAlignment(0);
-        titleLabel.setForeground(Color.red);
-        titlePanel.add(titleLabel);
-        
-        JButton startButton = new JButton("Start");
-        startButton.setLocation(650, 40);
-        startButton.setSize(75, 25);
-        titlePanel.add(startButton);
-        
-        
-        
-          //Label for text output
-          JPanel messagePanel = new JPanel();
-          messagePanel.setLayout(null);
-          messagePanel.setLocation(75, 125);
-          messagePanel.setSize(600, 400); //600 x 400
-          totalGUI.add(messagePanel);
-         
-         
-         JTextArea qTextArea = new JTextArea();
-         qTextArea.setText(message);
-         qTextArea.setWrapStyleWord(true);
-         qTextArea.setLineWrap(true);
-         qTextArea.setOpaque(false);
-         qTextArea.setEditable(false);
-         qTextArea.setFocusable(false);
-         qTextArea.setBackground(UIManager.getColor("Label.background"));
-         qTextArea.setFont(UIManager.getFont("Label.font"));
-         qTextArea.setBorder(UIManager.getBorder("Label.border"));
-         totalGUI.add(qTextArea);
-         
-         
-         
-         
-         // Label for question 
-         JLabel questionLabel = new JLabel(message);
-         questionLabel.setFont (questionLabel.getFont ().deriveFont (20.0f));
-         questionLabel.setLocation(25,0); // 0,0
-         questionLabel.setSize(600, 50);
-         questionLabel.setHorizontalAlignment(0);
-         questionLabel.setForeground(Color.blue);
-         messagePanel.add(questionLabel);
-         
-         
-         // Label for answerA
-         JLabel answerALabel = new JLabel("A: " + answerA);
-         answerALabel.setFont (questionLabel.getFont ().deriveFont (20.0f));
-         answerALabel.setLocation(0,150);
-         answerALabel.setSize(300, 50);
-         answerALabel.setHorizontalAlignment(0);
-         answerALabel.setForeground(Color.black);
-         messagePanel.add(answerALabel);
-         
-         // Label for answerB
-         JLabel answerBLabel = new JLabel("B: " + answerB);
-         answerBLabel.setFont (questionLabel.getFont ().deriveFont (20.0f));
-         answerBLabel.setLocation(0,300);
-         answerBLabel.setSize(300, 50);
-         answerBLabel.setHorizontalAlignment(0);
-         answerBLabel.setForeground(Color.black);
-         messagePanel.add(answerBLabel);
-         
-         // Label for answerCLabel 
-         JLabel answerCLabel = new JLabel("C: " + answerC);
-         answerCLabel.setFont (questionLabel.getFont ().deriveFont (20.0f));
-         answerCLabel.setLocation(300,150);
-         answerCLabel.setSize(300, 50);
-         answerCLabel.setHorizontalAlignment(0);
-         answerCLabel.setForeground(Color.black);
-         messagePanel.add(answerCLabel);
-         
-         // Label for answerDLabel 
-         JLabel answerDLabel = new JLabel("D: " + answerD);
-         answerDLabel.setFont (questionLabel.getFont ().deriveFont (20.0f));
-         answerDLabel.setLocation(300,300);
-         answerDLabel.setSize(300, 50);
-         answerDLabel.setHorizontalAlignment(0);
-         answerDLabel.setForeground(Color.black);
-         messagePanel.add(answerDLabel);
+		JTextArea questionLabel = new JTextArea();
+		questionLabel.setText(message);
+		questionLabel.setSize(570, 50);
+		questionLabel.setLocation(25, 0);
+		questionLabel.setWrapStyleWord(true);
+		questionLabel.setLineWrap(true);
+		questionLabel.setOpaque(false);
+		questionLabel.setEditable(false);
+		questionLabel.setFocusable(false);
+		questionLabel.setBackground(UIManager.getColor("Label.background"));
+		questionLabel.setFont(UIManager.getFont("Label.font"));
+		questionLabel.setBorder(UIManager.getBorder("Label.border"));
+		questionLabel.setFont(questionLabel.getFont().deriveFont(20.0f));
+		questionLabel.setForeground(Color.blue);
 
+		messagePanel.add(questionLabel);
 
+		// Label for answerA
+		JLabel answerALabel = new JLabel("A: " + answerA);
+		answerALabel.setFont(questionLabel.getFont().deriveFont(20.0f));
+		answerALabel.setLocation(0, 150);
+		answerALabel.setSize(300, 50);
+		answerALabel.setHorizontalAlignment(0);
+		answerALabel.setForeground(Color.black);
+		messagePanel.add(answerALabel);
 
-         // Label for score (money won)
-         JPanel scorePanel = new JPanel();
-         scorePanel.setLayout(null);
-         scorePanel.setLocation(200, 475);
-         scorePanel.setSize(350, 25);
-         totalGUI.add(scorePanel);
-         
-         JLabel scoreLabel = new JLabel("Money Won: $" + moneyWon + "!");
-         scoreLabel.setFont (scoreLabel.getFont ().deriveFont (25.0f));
-         scoreLabel.setLocation(0,0);
-         scoreLabel.setSize(350, 25);
-         scoreLabel.setHorizontalAlignment(0);
-         scoreLabel.setForeground(Color.red);
-         scorePanel.add(scoreLabel);
- 
-       
-        // Creation of a label to contain all the JButtons.
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(null);
-        buttonPanel.setLocation(50, 550);
-        buttonPanel.setSize(700, 200);
-        totalGUI.add(buttonPanel);
+		// Label for answerB
+		JLabel answerBLabel = new JLabel("B: " + answerB);
+		answerBLabel.setFont(questionLabel.getFont().deriveFont(20.0f));
+		answerBLabel.setLocation(0, 300);
+		answerBLabel.setSize(300, 50);
+		answerBLabel.setHorizontalAlignment(0);
+		answerBLabel.setForeground(Color.black);
+		messagePanel.add(answerBLabel);
 
-        // Buttons 
-        JButton aButton = new JButton("A");
-        aButton.setLocation(0, 0);
-        aButton.setSize(150, 75);
-        buttonPanel.add(aButton);
+		// Label for answerCLabel 
+		JLabel answerCLabel = new JLabel("C: " + answerC);
+		answerCLabel.setFont(questionLabel.getFont().deriveFont(20.0f));
+		answerCLabel.setLocation(300, 150);
+		answerCLabel.setSize(300, 50);
+		answerCLabel.setHorizontalAlignment(0);
+		answerCLabel.setForeground(Color.black);
+		messagePanel.add(answerCLabel);
 
-        JButton bButton = new JButton("B");
-        bButton.setLocation(170, 0);
-        bButton.setSize(150, 75);
-        buttonPanel.add(bButton);
+		// Label for answerDLabel 
+		JLabel answerDLabel = new JLabel("D: " + answerD);
+		answerDLabel.setFont(questionLabel.getFont().deriveFont(20.0f));
+		answerDLabel.setLocation(300, 300);
+		answerDLabel.setSize(300, 50);
+		answerDLabel.setHorizontalAlignment(0);
+		answerDLabel.setForeground(Color.black);
+		messagePanel.add(answerDLabel);
 
-        JButton cButton = new JButton("C");
-        cButton.setLocation(345, 0);
-        cButton.setSize(150, 75);
-        buttonPanel.add(cButton); 
-        
-        JButton dButton = new JButton("D");
-        dButton.setLocation(515, 0);
-        dButton.setSize(150, 75);
-        buttonPanel.add(dButton);
-        
-        
-        totalGUI.setOpaque(true);
-        return totalGUI;
-    }
+		// Label for score (money won)
+		JPanel scorePanel = new JPanel();
+		scorePanel.setLayout(null);
+		scorePanel.setLocation(200, 475);
+		scorePanel.setSize(350, 25);
+		totalGUI.add(scorePanel);
 
-    private static void showGUI() {
+		JLabel scoreLabel = new JLabel("Money Won: $" + moneyWon + "!");
+		scoreLabel.setFont(scoreLabel.getFont().deriveFont(25.0f));
+		scoreLabel.setLocation(0, 0);
+		scoreLabel.setSize(350, 25);
+		scoreLabel.setHorizontalAlignment(0);
+		scoreLabel.setForeground(Color.red);
+		scorePanel.add(scoreLabel);
 
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame frame = new JFrame("Who wants to be a Millionaire");
-        
-        
+		// Creation of a label to contain all the JButtons.
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(null);
+		buttonPanel.setLocation(50, 550);
+		buttonPanel.setSize(700, 200);
+		totalGUI.add(buttonPanel);
 
-        //Create and set up the content pane.
-        MillionaireGUI gui = new MillionaireGUI();
-        frame.setContentPane(gui.createContentPane());
+		// Buttons 
+		JButton aButton = new JButton("A");
+		aButton.setLocation(0, 0);
+		aButton.setSize(150, 75);
+		buttonPanel.add(aButton);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 700);
-        frame.setVisible(true);
-    }
+		JButton bButton = new JButton("B");
+		bButton.setLocation(170, 0);
+		bButton.setSize(150, 75);
+		buttonPanel.add(bButton);
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                showGUI();
-            }
-        });
-    }
+		JButton cButton = new JButton("C");
+		cButton.setLocation(345, 0);
+		cButton.setSize(150, 75);
+		buttonPanel.add(cButton);
+
+		JButton dButton = new JButton("D");
+		dButton.setLocation(515, 0);
+		dButton.setSize(150, 75);
+		buttonPanel.add(dButton);
+
+		totalGUI.setOpaque(true);
+		return totalGUI;
+	}
+
+	private static void showGUI()
+	{
+
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame frame = new JFrame("Who wants to be a Millionaire");
+
+		//Create and set up the content pane.
+		MillionaireGUI gui = new MillionaireGUI();
+		frame.setContentPane(gui.createContentPane());
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 700);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				showGUI();
+			}
+		});
+	}
 }
