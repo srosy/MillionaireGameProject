5c1537d690692a47afccef83882b50c88b7ae76b
Spencer Rosenvall <serosenvall@gmail.com>
2017-11-26 12:34:16 -0700

    Update Price.java
    -added reset price method

--- Price.java
+++ Price.java
@@ -1,15 +1,18 @@
 public class Price {
-   private int priceIndex;
-   public String[] winnerPrice = {"$0", "$100", "$200", "$300", "$500", "$1000", "$2000",
-                                  "$4000", "$8000", "$16000", "$32000", "$64000",
-                                  "$125000", "$250000", "$500000", "$1_Million"};
-                                  
-   public void setPrice() {
-      priceIndex++;
-   }
-   
-   public String getPrice() {
-      return winnerPrice[priceIndex];
-   }
-   
+	private int priceIndex;
+	public String[] winnerPrice = { "0", "500", "1,000", "2,000", "4,000", "8,000", "16,000", "32,000", "64,000",
+			"125,000", "250,000", "500,000", "1,000,000" };
+
+	public void setPrice() {
+		priceIndex++;
+	}
+
+	public void resetPrice() {
+		priceIndex = 0;
+	}
+
+	public String getPrice() {
+		return winnerPrice[priceIndex];
+	}
+
 }
